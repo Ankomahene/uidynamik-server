a29635c13c48d509ddd3294bbe66055d625a46fd
ankomahene <ankomaheneshadrack@gmail.com>
2020-07-25 13:34:10 +0000

    changes filename

--- app/controllers/s3.controller.js
+++ app/controllers/s3.controller.js
@@ -6,13 +6,13 @@ exports.doDownload = (req, res) => {
 	const s3Client = s3.s3Client;
 	const params = s3.downloadParams;
 
-	params.Key = `${req.params.id}-${req.query.filename}`;
+	params.Key = `${req.params.id}-${req.query.filename}.zip`;
 
 	s3Client
 		.getObject(params)
 		.createReadStream()
 		.on('error', function(err) {
-			res.redirect('/not-found');
+			res.json({ error: 'file not found' });
 		})
 		.pipe(res);
 };
--- server.js
+++ server.js
@@ -5,13 +5,6 @@ const path = require('path');
 let router = require('./app/routers/s3.router.js');
 app.use('/', router);
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-// app.get('*', (req, res) => {
-// 	res.sendFile(path.join(__dirname + '/client/build/index.html'));
-// });
-
 // Create a Server
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`server has started on port: ${port}`));
