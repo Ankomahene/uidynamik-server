654ca69803ea1a4df056492f53299a7a427a0fdc
ankomahene <ankomaheneshadrack@gmail.com>
2020-07-31 16:09:14 +0000

    added environment file

--- app/config/s3.config.js
+++ app/config/s3.config.js
@@ -1,19 +1,19 @@
 const AWS = require('aws-sdk');
 const env = require('./s3.env.js');
- 
+
 const s3Client = new AWS.S3({
-    accessKeyId: env.AWS_ACCESS_KEY,
-    secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
-	region : env.REGION
+	accessKeyId: process.env.AWS_ACCESS_KEY,
+	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+	region: process.env.REGION
 });
- 
+
 const downloadParams = {
-         Bucket: env.Bucket, 
-         Key: '', // pass key
+	Bucket: env.Bucket,
+	Key: ''
 };
 
 const s3 = {};
 s3.s3Client = s3Client;
 s3.downloadParams = downloadParams;
- 
+
 module.exports = s3;
